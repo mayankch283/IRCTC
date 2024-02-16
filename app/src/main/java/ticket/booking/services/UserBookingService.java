eb76e5d3177fd83119781c9a7bc51d4065045dc6
mayankch283 <mayankch283@gmail.com>
2024-02-17 01:19:44 +0530

    <feat>: added objectmapper, loginUser and signup in UserBookingService

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,4 +1,49 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.w3c.dom.stylesheets.LinkStyle;
+import ticket.booking.entities.User;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Optional;
+
 public class UserBookingService {
+    private User user;
+
+    private List<User> userList;
+
+    private static final String USERS_PATH = "../localDb/users.json";
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public UserBookingService(User user) throws IOException {
+        this.user = user;
+        File users  = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+
+    }
+
+
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1->{
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword())
+        }).findFirst();
+        return foundUser.isPresent();
+
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }
+        catch(IOException ex){
+            return Boolean.FALSE;
+        }
+
+    }
 }
