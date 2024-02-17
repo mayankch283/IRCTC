2e4641ec7cf9325723bbe75f54d3d31afdfecbb4
mayankch283 <mayankch283@gmail.com>
2024-02-17 14:46:05 +0530

    <feat>: added saveUserListToFile(), fetchBooking() and cancelBooking()

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -3,12 +3,16 @@ package ticket.booking.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.w3c.dom.stylesheets.LinkStyle;
+import ticket.booking.entities.Ticket;
 import ticket.booking.entities.User;
+import ticket.booking.util.UserServiceUtil;
+
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class UserBookingService {
     private User user;
@@ -46,4 +50,37 @@ public class UserBookingService {
         }
 
     }
+
+    private void saveUserListToFile() throws IOException{
+        File usersFile = new File(USERS_PATH);
+        objectMapper.writeValue(usersFile, userList);
+    }
+
+    public void fetchBooking(){
+        user.printTickets();
+    }
+
+    //Need to test this function
+    public Boolean cancelBooking(String ticketId){
+        if(user.getTicketsBooked().contains(ticketId)){
+
+            List<Ticket> newBookedTickets = user.getTicketsBooked().stream().filter(ticketsBooked->{
+                return !(user.getTicketsBooked().equals(ticketId));
+            }).collect(Collectors.toList());
+
+            user.setTicketsBooked(newBookedTickets);
+
+            try {
+                saveUserListToFile();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+
+            return Boolean.TRUE;
+        }
+        else {
+            return Boolean.FALSE;
+        }
+    }
 }
+
