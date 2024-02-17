6b9bf9da8ea71b623ec243571a9130c089f3825e
mayankch283 <mayankch283@gmail.com>
2024-02-17 11:32:28 +0530

    <feat>: added getters and setters for User entity

--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -7,5 +7,16 @@ public class User {
     private String hashedPassword;
     private List<Ticket> ticketsBooked;
     private String userId;
-    
+
+
+    public String getName(){ return name; }
+    public String getPassword(){ return password; }
+    public String getHashedPassword(){ return hashedPassword;}
+    public List<Ticket> getTicketsBooked(){ return ticketsBooked; }
+    public String getUserId(){ return userId; }
+
+    public void setName(String name){ this.name = name; }
+    public void setHashedPassword(String hashedPassword){ this.hashedPassword = hashedPassword; }
+    public void setTicketsBooked(List<Ticket> ticketsBooked){ this.ticketsBooked = ticketsBooked; }
+    public void setUserId(String userId){ this.userId = userId; }
 }
